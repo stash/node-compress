bbc595fecf9089dde301c2cece7690049ac4febd
Ivan (egorich) Egorov <egorich.3.04@gmail.com>
2010-05-08 18:23:16 +0400

    ScopedBlob implemented to encapsulate shrinking buffer.

--- utils.h
+++ utils.h
@@ -26,5 +26,68 @@ class ScopedArray {
   ScopedArray& operator=(const ScopedArray&);
 };
 
+
+template <class T>
+class ScopedOutputBuffer {
+ public:
+  ScopedOutputBuffer() 
+    : data_(0), length_(0)
+  {}
+
+  ScopedOutputBuffer(size_t initial)
+    : data_(0), length_(0)
+  {
+    GrowTo(initial);
+  }
+
+  ~ScopedOutputBuffer() {
+    Free();
+  }
+
+  bool GrowBy(size_t sz) {
+    return GrowTo(length_ + sz);
+  }
+
+
+  bool GrowTo(size_t sz) {
+    if (sz == 0) {
+      return true;
+    }
+
+    T *tmp = (T*) realloc(data_, sz * sizeof(T));
+    if (tmp == NULL) {
+      return false;
+    }
+    data_ = tmp;
+    length_ = sz;
+    return true;
+  }
+
+  void Free() {
+    free(data_);
+    data_ = 0;
+    length_ = 0;
+  }
+
+  T* data() const {
+    return data_;
+  }
+
+  size_t length() const {
+    return length_;
+  }
+ 
+ private:
+  T* data_;
+  size_t length_;
+
+ private:
+  ScopedOutputBuffer(ScopedOutputBuffer&);
+  ScopedOutputBuffer(const ScopedOutputBuffer&);
+  ScopedOutputBuffer& operator=(ScopedOutputBuffer&);
+  ScopedOutputBuffer& operator=(const ScopedOutputBuffer&);
+};
+
+typedef ScopedOutputBuffer<char> ScopedBlob;
 #endif
 
