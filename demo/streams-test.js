021d0593721cc8bfea113086f0f37c56543a3a2f
Ivan (egorich) Egorov <egorich.3.04@gmail.com>
2010-06-11 23:40:59 +0400

    Fixed some end of stream issue.

--- demo/streams-test.js
+++ demo/streams-test.js
@@ -3,43 +3,49 @@ var sys=require("sys");
 var posix=require("fs");
 var Buffer = require('buffer').Buffer;
 
-sys.puts('Compressing...');
-var gzdata = '';
-var gzip=new compress.GzipStream();
-gzip.setInputEncoding('utf8');
-gzip.setEncoding('binary');
-gzip.addListener('data', function(data) {
-  gzdata += data;
-}).addListener('error', function(err) {
-  throw err;
-}).addListener('end', function() {
-  sys.puts('Compressed length: ' + gzdata.length);
-});
+function doCompress(continuation) {
+  sys.puts('Making compression requests...');
+  var gzdata = '';
+  var gzip=new compress.GzipStream();
+  gzip.setInputEncoding('utf8');
+  gzip.setEncoding('binary');
+  gzip.addListener('data', function(data) {
+    gzdata += data;
+  }).addListener('error', function(err) {
+    throw err;
+  }).addListener('end', function() {
+    sys.puts('Compressed length: ' + gzdata.length);
+    continuation(gzdata);
+  });
 
-gzip.write("My data that needs ");
-gzip.write("to be compressed. 01234567890.");
-gzip.close();
-sys.puts('Compression finished.');
+  gzip.write("My data that needs ");
+  gzip.write("to be compressed. 01234567890.");
+  gzip.close();
+  sys.puts('Requests done.');
+}
 
-var d1 = gzdata.substr(0, 25);
-var d2 = gzdata.substr(25);
+function doDecompress(gzdata) {
+  var d1 = gzdata.substr(0, 25);
+  var d2 = gzdata.substr(25);
 
+  sys.puts('Making decompression requests...');
+  var rawdata = '';
+  var gunzip = new compress.GunzipStream();
+  gunzip.setInputEncoding('binary');
+  gunzip.setEncoding('utf8');
+  gunzip.addListener('data', function(data) {
+    rawdata += data;
+  }).addListener('error', function(err) {
+    throw err;
+  }).addListener('end', function() {
+    sys.puts('Decompressed length: ' + rawdata.length);
+    sys.puts('Raw data: ' + rawdata);
+  });
+  gunzip.write(d1);
+  gunzip.write(d2);
+  gunzip.close();
+  sys.puts('Requests done.');
+}
 
-sys.puts('Decompressing...');
-var rawdata = '';
-var gunzip = new compress.GunzipStream();
-gunzip.setInputEncoding('binary');
-gunzip.setEncoding('utf8');
-gunzip.addListener('data', function(data) {
-  rawdata += data;
-}).addListener('error', function(err) {
-  throw err;
-}).addListener('end', function() {
-  sys.puts('Decompressed length: ' + rawdata.length);
-  sys.puts('Raw data: ' + rawdata);
-});
-gunzip.write(d1);
-gunzip.write(d2);
-gunzip.close();
-sys.puts('Decompression finished.');
+doCompress(doDecompress);
 
